1af798fdce8f1ba8d4078b00c5201c923460f4de
juancanas45 <84482151+juancanas45@users.noreply.github.com>
2021-11-19 15:16:20 -0500

    Update src.js
    2

--- src.js
+++ src.js
@@ -1,5 +1,14 @@
 const calcularDepreciacionNIIF =(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar)=>{
-
+  if (vidaUtil>=0){
+              return 0;
+        }
+  if (numeroPeriodoAconsultar>vidaUtil){
+              return precioFinal;
+        }
+  
+ var dep=(precioInicial-precioFinal)/vidaUtil;
+  return dep; 
+ 
 
 
 
