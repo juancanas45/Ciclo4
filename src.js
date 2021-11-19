8547cb11371b955e2ee73c14b09a44c223f409bf
juancanas45 <84482151+juancanas45@users.noreply.github.com>
2021-11-19 15:03:07 -0500

    Update src.js
    jjca

---
+++ src.js
@@ -0,0 +1,13 @@
+const calcularDepreciacionNIIF =(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar)=>{
+
+
+
+
+}
+
+const calcularDepreciacionNIIFEnDolares=(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar)=>{
+
+}
+
+modelu.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
+modelu.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
