04fcdc7d32e6f29bb0fced6bb598cc0668b9b261
Juan Torres <juanctd@gmail.com>
2021-11-19 22:05:24 -0500

    Linea Base Ciclo4, segundo  Commit

---
+++ src.js
@@ -0,0 +1,16 @@
+
+
+const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+
+//insert logica aqui
+
+}
+
+const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+
+
+    
+}
+
+module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
+module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
