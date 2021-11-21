524a522a40183e8fd3931b283d9aa6316a68d50e
juancanas45 <84482151+juancanas45@users.noreply.github.com>
2021-11-21 12:52:19 -0500

    Update src.js
    final con 100% resuelto

--- src.js
+++ src.js
@@ -1,16 +1,46 @@
 
 
-const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+const calcularDepreciacionNIIF =(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar)=>{
+  
+  if (vidaUtil === 0){
+              return 0;
+        }
+  
+  if (vidaUtil < 0){
+              return 0;
+        }
+  
+  
+  if (numeroPeriodoAconsultar > vidaUtil){
+              return precioFinal;
+        }
+  
+ var dep=((precioInicial-precioFinal)/vidaUtil);   
+ var dep1= dep*numeroPeriodoAconsultar;
+ var depf = precioInicial-dep1;
 
-//insert logica aqui
-
+  return depf; 
+ 
 }
+const calcularDepreciacionNIIFEnDolares=(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar)=>{
 
-const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+  if (precioInicial < 0){
+             throw "Error";
+        }
+  
+  
+  
+ var dep=((precioInicial-precioFinal)/vidaUtil);   
+ var dep1= dep*numeroPeriodoAconsultar;
+ var depf = precioInicial-dep1;
+ var deprd=(depf/3778)
 
+ if (Math.sign(deprd) < 0){
+              throw "Error";
+        }
 
-    
+  return deprd; 
+  
 }
-
 module.exports.calcularDepreciacionNIIF = calcularDepreciacionNIIF;
 module.exports.calcularDepreciacionNIIFEnDolares = calcularDepreciacionNIIFEnDolares;
